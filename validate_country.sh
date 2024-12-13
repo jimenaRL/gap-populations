6fb7eb2fed9fca2424579f03016421de6867b1f2
Jimena Royo-Letelier <jimena@melix.net>
2024-12-13 11:02:20 +0000

    Fix wrong parameter for israel validation

--- validate_country.sh
+++ validate_country.sh
@@ -188,7 +188,7 @@ function fn() {
             --year=$YEAR \
             --dbpath=$DBPATH \
             --config=/home/jimena/work/dev/gap-populations/configs/embeddings_israel.yaml \
-            --SURVEY=gps2019 \
+            --survey=gps2019 \
             --attdims=V4_Scale,V6_Scale,V8_Scale,V9,V10,V12,V13,V14,V18,V19,V20,V21 \
             --validation
     fi
