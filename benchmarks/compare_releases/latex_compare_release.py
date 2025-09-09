f43d5b4f0123554970e0cb9e8778064cdda4062c
jimenaRL <jimena@melix.net>
2025-09-09 17:44:02 +0200

    WIP validation to tex

--- benchmarks/compare_releases/latex_compare_release.py
+++ benchmarks/compare_releases/latex_compare_release.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser
 
 ap = ArgumentParser(prog="Make openia async requests.")
 ap.add_argument('--country', required=True, type=str)
-ap.add_argument('--year', required=True, type=int, choices=[2023, 2023])
+ap.add_argument('--year', required=True, type=int, choices=[2023, 2023, 2025])
 
 args = ap.parse_args()
 country = args.country
@@ -15,16 +15,20 @@ year = args.year
 r1 = "20250416"
 r2 = "next"
 alias = {
-    "r1": "free_answer",
-    "r2": "guidance_choice"
+    "r1": "free",
+    "r2": "guided"
+}
+validation_patterns = {
+    "r1": "/mnt/hdd2/epodata/production/v0/validations/${country}_${year}_${survey}_logistic_regression_cross_validate_f1_score_*.csv",
+    "r2": "/mnt/hdd1/jimena/epodata/stage/next/validations/${country}_${year}_${survey}_logistic_regression_cross_validate_f1_score.csv"
 }
 
 s1 = alias["r1"]
 s2 = alias["r2"]
 outfile = f"benchmark_{country}_{year}_{s1}_vs_{s2}.txt"
 
-validation_pattern = "/mnt/hdd2/epodata/stage/${release}/validations/${country}_${year}_${survey}_logistic_regression_cross_validate_f1_score.csv"
-validation_pattern = Template(validation_pattern).safe_substitute(country=country, year=year)
+# validation_pattern = "/mnt/hdd2/epodata/stage/${release}/validations/${country}_${year}_${survey}_logistic_regression_cross_validate_f1_score.csv"
+# validation_pattern = Template(validation_pattern).safe_substitute(country=country, year=year)
 
 section = "\section{${country}}"
 
@@ -108,33 +112,40 @@ def getLatexTables(country, year, metric):
     return df, tables
 
 
-validations_paths = glob(Template(validation_pattern).substitute(release=r1, survey='*'))
-surveys = [p.split(f"{country}_{year}_")[1].split('_')[0] for p in validations_paths]
+if __name__ == "__main__":
 
 
-latex = Template(section).safe_substitute(country=country.capitalize())
-latex += "\n\n"
-for survey in surveys:
-    df1 = pd.read_csv(Template(validation_pattern).substitute(release=r1, survey=survey))[cols] \
-        .rename(columns={k:v for k,v in zip(cols, renamed_cols)}) \
-        .assign(strategy=alias["r1"])
-    df2 = pd.read_csv(Template(validation_pattern).substitute(release=r2, survey=survey))[cols] \
-        .rename(columns={k:v for k,v in zip(cols, renamed_cols)}) \
-        .assign(strategy=alias["r2"])
+    validations_paths = {
+        k: glob(Template(v).substitute(survey='*')) for k,v in  validation_patterns.items()
+    }
 
-    df = pd.concat([df2, df1]) \
-        .sort_values(["att_dim", "l2", "l1"]) \
-        .drop(columns=["country", "survey"])
 
-    latex += Template(subsection).safe_substitute(
-        country=country.capitalize(),
-        year=year,
-        survey=survey)
+    surveys = [p.split(f"{country}_{year}_")[1].split('_')[0] for p in validations_paths]
+
+
+    latex = Template(section).safe_substitute(country=country.capitalize())
     latex += "\n\n"
-    latex += frame2Latex(df)
+    for survey in surveys:
+        df1 = pd.read_csv(Template(validation_pattern).substitute(release=r1, survey=survey))[cols] \
+            .rename(columns={k:v for k,v in zip(cols, renamed_cols)}) \
+            .assign(strategy=alias["r1"])
+        df2 = pd.read_csv(Template(validation_pattern).substitute(release=r2, survey=survey))[cols] \
+            .rename(columns={k:v for k,v in zip(cols, renamed_cols)}) \
+            .assign(strategy=alias["r2"])
+
+        df = pd.concat([df2, df1]) \
+            .sort_values(["att_dim", "l2", "l1"]) \
+            .drop(columns=["country", "survey"])
+
+        latex += Template(subsection).safe_substitute(
+            country=country.capitalize(),
+            year=year,
+            survey=survey)
+        latex += "\n\n"
+        latex += frame2Latex(df)
 
 
-with open(outfile, 'w') as f:
-    f.write(latex)
+    with open(outfile, 'w') as f:
+        f.write(latex)
 
-print(f"Latex code save at {outfile}")
+    print(f"Latex code save at {outfile}")
